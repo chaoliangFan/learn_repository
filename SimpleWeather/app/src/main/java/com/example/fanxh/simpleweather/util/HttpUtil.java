535793802a4bf857408c6777e899959b9f383d86
fanxh <2295471337@qq.com>
2017-10-23 20:42:41 +0800

    待修改

---
+++ SimpleWeather/app/src/main/java/com/example/fanxh/simpleweather/util/HttpUtil.java
@@ -0,0 +1,18 @@
+package com.example.fanxh.simpleweather.util;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by fanxh on 2017/10/23.
+ */
+
+public class HttpUtil {
+    public static void sendOkHttpRequest(String address,okhttp3.Callback callback){
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(address)
+                .build();
+        client.newCall(request).enqueue(callback);
+    }
+}
