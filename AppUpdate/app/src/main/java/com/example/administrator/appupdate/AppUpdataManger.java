1429e6cfc9419158a1b3ce7a8ad73a10544c59ea
fanxh <2295471337@qq.com>
2017-10-25 19:13:48 +0800

    finishing

--- AppUpdate/app/src/main/java/com/example/administrator/appupdate/AppUpdataManger.java
+++ AppUpdate/app/src/main/java/com/example/administrator/appupdate/AppUpdataManger.java
@@ -3,11 +3,13 @@ package com.example.administrator.appupdate;
 import android.app.DownloadManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Environment;
+import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.webkit.MimeTypeMap;
 import android.widget.Toast;
@@ -17,11 +19,10 @@ import android.widget.Toast;
  */
 
 public class AppUpdataManger {
-    private DownloadManager downloadManager;
+    private static DownloadManager downloadManager;
     private Context mContext;
-    private long mTaskId;
+    public static long mTaskId;
     private String versionName;
-    public static int i = 0;
 
     public AppUpdataManger(Context context) {
         this.mContext = context;
@@ -35,35 +36,23 @@ public class AppUpdataManger {
         }
     };
 
-    //使用系统下载器下载
     public void downloadAPK(String versionUrl, String versionName) {
         this.versionName = versionName;
-        //将下载请求加入下载队列
         downloadManager = (DownloadManager) mContext.getSystemService(Context.DOWNLOAD_SERVICE);
-        //创建下载任务
         DownloadManager.Request request = new DownloadManager.Request(Uri.parse(versionUrl));
-        request.setAllowedOverRoaming(false);//漫游网络是否可以下载
-        //设置文件类型，可以在下载结束后自动打开该文件
+        request.setAllowedOverRoaming(false);
         MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
         String mimeString = mimeTypeMap.getMimeTypeFromExtension(MimeTypeMap.getFileExtensionFromUrl(versionUrl));
-        request.setMimeType(mimeString);//加入任务队列
-        //在通知栏显示，默认就是显示的
+        request.setMimeType(mimeString);
         request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE);
         request.setAllowedOverRoaming(false);
         request.setVisibleInDownloadsUi(true);
-//       sdcard的目录下的download文件夹，必须设置
-//       request.setDestinationInExternalPublicDir("/sdcard/download", versionName);
-//       自定义路径的方法
         request.setDestinationInExternalFilesDir(mContext, Environment.DIRECTORY_DOWNLOADS, versionName);
-        //加入下载列后会给该任务返回一个long型的id,
-        //通过该id可以取消任务，重启任务等等
         mTaskId = downloadManager.enqueue(request);
-        //注册广播接收，监听下载状态
         mContext.registerReceiver(receiver, new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE));
     }
 
-    //检查下载状态
-    private void checkDownloadStatus() {
+    public void checkDownloadStatus() {
         DownloadManager.Query query = new DownloadManager.Query();
         query.setFilterById(mTaskId);
         Cursor cursor = downloadManager.query(query);
@@ -71,26 +60,38 @@ public class AppUpdataManger {
             int status = cursor.getInt(cursor.getColumnIndex(DownloadManager.COLUMN_STATUS));
             switch (status) {
                 case DownloadManager.STATUS_SUCCESSFUL:
-                    Toast.makeText(mContext, "下载完成", Toast.LENGTH_LONG).show();
-                    i =i +1;
-                    //打开文件进行安装
-                    installAPK(mTaskId);
+                    AlertDialog.Builder dialog = new AlertDialog.Builder(mContext);
+                    dialog.setTitle("下载完成");
+                    dialog.setMessage("是否立即安装");
+                    dialog.setCancelable(true);
+                    dialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            installAPK(mTaskId);
+                        }
+                    });
+                    dialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                        }
+                    });
+                    dialog.show();
                     break;
                 case DownloadManager.STATUS_FAILED:
                     Toast.makeText(mContext, "更新失败", Toast.LENGTH_LONG).show();
+                    break;
+                default:
                     break;
             }
         }
         cursor.close();
     }
 
-    //下载到本地后执行安装根据获得的id进行安装
-    protected void installAPK(long appId) {
+    public void installAPK(long appId) {
         Intent install = new Intent(Intent.ACTION_VIEW);
-        Uri downloadFileUri = downloadManager.getUriForDownloadedFile(appId);
+         Uri downloadFileUri = downloadManager.getUriForDownloadedFile(appId);
         install.setDataAndType(downloadFileUri, "application/vnd.android.package-archive");
         install.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         mContext.startActivity(install);
-        i=0;
     }
 }
--- AppUpdate/app/src/main/java/com/example/administrator/appupdate/MainActivity.java
+++ AppUpdate/app/src/main/java/com/example/administrator/appupdate/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.administrator.appupdate;
 
+import android.content.Context;
 import android.content.DialogInterface;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.ActionBar;
@@ -15,14 +19,19 @@ import android.widget.Toast;
 
 import org.json.JSONObject;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+
 public class MainActivity extends AppCompatActivity {
     private static final String UPDATEURL = "http://update.app.2345.com/index.php";
-    private InformationData informationData = new InformationData();
+    private InformationData mInformationData;
     ;
     private AppUpdataManger appUpdataManger;
     private LinearLayout mVersionTest;
     private TextView mShowVersionName;
-    private Handler handler = new Handler() {
+    private Handler receiveMessage = new Handler() {
         public void handleMessage(Message msg) {
             mShowDialog(msg.what);
         }
@@ -42,26 +51,32 @@ public class MainActivity extends AppCompatActivity {
         mVersionTest.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                HttpUtil.sendHttpRequest(UPDATEURL, new HttpCallBackListener() {
-                    @Override
-                    public void onFinish(String response) {
-                        parseJsonWithJsonObject(response);
-                        if (!TextUtils.isEmpty(informationData.getNeed_update())) {
-                            Message message = new Message();
-                            message.what = 1;
-                            handler.sendMessage(message);
-                        } else {
-                            Message message = new Message();
-                            message.what = 2;
-                            handler.sendMessage(message);
+                ConnectivityManager connectionManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+                NetworkInfo networkInfo = connectionManager.getActiveNetworkInfo();
+                if (networkInfo == null || !networkInfo.isAvailable()) {
+                    Toast.makeText(MainActivity.this, "当前无网络，请先检查网络", Toast.LENGTH_SHORT).show();
+                } else {
+                    HttpUtil.sendHttpRequest(UPDATEURL, new HttpCallBackListener() {
+                        @Override
+                        public void onFinish(String response) {
+                            parseJsonWithJsonObject(response);
+                            if (!TextUtils.isEmpty(mInformationData.getNeed_update())) {
+                                Message message = new Message();
+                                message.what = 1;
+                                receiveMessage.sendMessage(message);
+                            } else {
+                                Message message = new Message();
+                                message.what = 2;
+                                receiveMessage.sendMessage(message);
+                            }
                         }
-                    }
 
-                    @Override
-                    public void onError(Exception e) {
-                        Toast.makeText(MainActivity.this, "服务器无响应", Toast.LENGTH_SHORT).show();
-                    }
-                });
+                        @Override
+                        public void onError(Exception e) {
+                            Toast.makeText(MainActivity.this, "服务器无响应", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
             }
         });
     }
@@ -69,18 +84,19 @@ public class MainActivity extends AppCompatActivity {
     private void parseJsonWithJsonObject(String jsonData) {
         try {
             JSONObject jsonObject = new JSONObject(jsonData);
-            informationData.setAppkey(jsonObject.getString("appkey"));
-            informationData.setChannel(jsonObject.getString("channel"));
-            informationData.setDownurl(jsonObject.getString("downurl"));
-            informationData.setPackname(jsonObject.getString("packname"));
-            informationData.setFilename(jsonObject.getString("filename"));
-            informationData.setFilesize(jsonObject.getString("filesize"));
-            informationData.setMd5(jsonObject.getString("md5"));
-            informationData.setVersion(jsonObject.getString("version"));
-            informationData.setUser_version(jsonObject.getString("user_version"));
-            informationData.setUpdatelog(jsonObject.getString("updatelog"));
-            informationData.setUpdatetype(jsonObject.getString("updatetype"));
-            informationData.setNeed_update(jsonObject.getString("need_pudate"));
+            mInformationData = new InformationData();
+            mInformationData.setAppkey(jsonObject.getString("appkey"));
+            mInformationData.setChannel(jsonObject.getString("channel"));
+            mInformationData.setDownurl(jsonObject.getString("downurl"));
+            mInformationData.setPackname(jsonObject.getString("packname"));
+            mInformationData.setFilename(jsonObject.getString("filename"));
+            mInformationData.setFilesize(jsonObject.getString("filesize"));
+            mInformationData.setMd5(jsonObject.getString("md5"));
+            mInformationData.setVersion(jsonObject.getString("version"));
+            mInformationData.setUser_version(jsonObject.getString("user_version"));
+            mInformationData.setUpdatelog(jsonObject.getString("updatelog"));
+            mInformationData.setUpdatetype(jsonObject.getString("updatetype"));
+            mInformationData.setNeed_update(jsonObject.getString("need_pudate"));
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -100,13 +116,22 @@ public class MainActivity extends AppCompatActivity {
         } else {
             AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
             dialog.setTitle("新版本更新");
-            dialog.setMessage(informationData.getUpdatelog());
+            dialog.setMessage(mInformationData.getUpdatelog());
             dialog.setCancelable(false);
             dialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
-                    appUpdataManger = new AppUpdataManger(MainActivity.this);
-                    appUpdataManger.downloadAPK(informationData.getDownurl(), informationData.getFilename());
+                    File downLoadFile = new File(getSystemFilePath(MainActivity.this));
+                    if (getDirMD5(downLoadFile, mInformationData.getMd5())) {
+                        Toast.makeText(MainActivity.this, "本地存在", Toast.LENGTH_SHORT).show();
+//                        Intent install = new Intent(Intent.ACTION_VIEW);
+//                        install.setDataAndType( Uri.fromFile(downLoadFile), "application/vnd.android.package-archive");
+//                        install.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//                        startActivity(install);
+                    } else {
+                        appUpdataManger = new AppUpdataManger(MainActivity.this);
+                        appUpdataManger.downloadAPK(mInformationData.getDownurl(), mInformationData.getFilename());
+                    }
                 }
             });
             dialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
@@ -116,6 +141,66 @@ public class MainActivity extends AppCompatActivity {
             });
             dialog.show();
         }
+    }
+
+    public static String getFileMD5(File file) {
+        if (!file.isFile()) {
+            return null;
+        }
+        MessageDigest digest = null;
+        FileInputStream in = null;
+        byte buffer[] = new byte[1024];
+        int len;
+        try {
+            digest = MessageDigest.getInstance("MD5");
+            in = new FileInputStream(file);
+            while ((len = in.read(buffer, 0, 1024)) != -1) {
+                digest.update(buffer, 0, len);
+            }
+            in.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+        BigInteger bigInt = new BigInteger(1, digest.digest());
+        return bigInt.toString(16);
+    }
+
+    /**
+     * @param dir               下载目录
+     * @param toDownloadFileMD5
+     * @return
+     */
+    public boolean getDirMD5(File dir, String toDownloadFileMD5) {
+        boolean isSameFile = false;
+        if (!dir.isDirectory()) {
+            isSameFile = false;
+        } else {
+            String md5;
+            File files[] = dir.listFiles();
+            for (int i = 0; i < files.length; i++) {
+                File f = files[i];
+                if (f.isDirectory()) {
+                    getDirMD5(f, toDownloadFileMD5);
+                } else {
+                    md5 = getFileMD5(f);
+                    if (md5.equals(toDownloadFileMD5)) {
+                        isSameFile = true;
+                    }
+                }
+            }
+        }
+        return isSameFile;
+    }
+
+    private static String getSystemFilePath(Context context) {
+        String cachePath;
+        if (Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState()) || !Environment.isExternalStorageRemovable()) {
+            cachePath = context.getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS).getAbsolutePath();
+        } else {
+            cachePath = context.getFilesDir().getAbsolutePath();
+        }
+        return cachePath;
     }
 }
 
